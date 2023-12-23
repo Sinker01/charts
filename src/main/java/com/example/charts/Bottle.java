05a795467a56d19cfc69d8b56370dd6684d2739d
Sven Kretschmer <149303655+Sinker01@users.noreply.github.com>
2023-12-23 13:18:15 +0100

    Soll- und Ist-Label werden korrekt angezeigt

--- src/main/java/com/example/charts/Bottle.java
+++ src/main/java/com/example/charts/Bottle.java
@@ -1,27 +1,36 @@
 package com.example.charts;
 
 import javafx.scene.Group;
+import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.LinearGradient;
 import javafx.scene.paint.Stop;
 import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
-
-import java.util.Arrays;
+import javafx.scene.text.Font;
 
 public class Bottle extends Group {
     private final Shape clone;
+    private int ist=100, soll=100;
+    private Label istLabel = new Label(Integer.toString(ist)),
+    sollLabel = new Label(Integer.toString(soll));
 
     public Bottle() {
         Polygon triangle = new Polygon();
-        triangle.getPoints().addAll(new Double[] {
-                310., 100.,
+        triangle.getPoints().addAll(310., 100.,
                 10., 600.,
-                610., 600.
-        });
+                610., 600.);
 
         Rectangle rectangle = new Rectangle(210, 10, 200, 400);
+
+        sollLabel.setTranslateX(50);
+        sollLabel.setFont(new Font(30));
+        sollLabel.setStyle("-fx-font-weight: bold");
+
+
+        istLabel.setFont(new Font(30));
+        istLabel.setStyle("-fx-font-weight: bold");
 
         // Combine the triangle and rectangle using Shape::union
         Shape combinedShape = Shape.union(triangle, rectangle);
@@ -32,6 +41,7 @@ public class Bottle extends Group {
                 new Stop(1, Color.RED));
 
         combinedShape.setFill(gradient);
+        istLabel.setTextFill(Color.BLUE);
 
         // Set the fill color
 
@@ -39,22 +49,43 @@ public class Bottle extends Group {
         combinedShape.setStroke(Color.BLACK);
         combinedShape.setStrokeWidth(20);
 
-        setPercent(0.5);
-
         clone.setStroke(Color.BLACK);
         clone.setStrokeWidth(20);
 
         // Add the combined shape to the group
         super.getChildren().add(combinedShape);
         super.getChildren().add(clone);
+        super.getChildren().add(sollLabel);
+        super.getChildren().add(istLabel);
     }
 
-    public void setPercent(double d) {
+    private void setPercent() {
+        double d = (double) ist/soll;
         LinearGradient fill = new LinearGradient(100, 20, 100, 620, false, null,
                 new Stop(0.0, Color.WHITE),
                 new Stop(1-d, Color.WHITE),
                 new Stop(1-d, Color.TRANSPARENT),
                 new Stop(1, Color.TRANSPARENT));
         clone.setFill(fill);
+
+    }
+
+    public void setSoll(int soll) {
+        this.soll=soll;
+        setPercent();
+        sollLabel.setText("Soll:\t"  + Integer.toString(soll));
+    }
+
+    public void setIst(int ist) {
+        this.ist = ist;
+        setPercent();
+        double d = (double) ist/soll;
+        double y = 570 - (d) * 570;
+        double x = 500;
+        if(y>200) x += (y-200)/400. * 200;
+
+        istLabel.setTranslateX(x);
+        istLabel.setTranslateY(y);
+        istLabel.setText("Ist:\t" + Integer.toString(ist));
     }
 }
