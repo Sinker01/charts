2e72afe2bb9f2ce1aa4c10e987f6e3a2a77c7cc7
Sven Kretschmer <149303655+Sinker01@users.noreply.github.com>
2024-03-07 18:23:14 +0100

    Main-Klassen gelöscht

--- src/main/java/sit/app/factory/charts/BottleMain.java
+++
@@ -1,9 +0,0 @@
-package sit.app.factory.charts;
-/**
- * Main-Klasse, um das Flache-Programm zu starten
- */
-public class BottleMain {
-    public static void main(String[] args) {
-        BottleApplication.main(args);
-    }
-}
--- src/main/java/sit/app/factory/charts/GraphMain.java
+++
@@ -1,7 +0,0 @@
-package sit.app.factory.charts;
-
-public class GraphMain {
-    public static void main(String[] args) {
-        GraphApplication.main(args);
-    }
-}
